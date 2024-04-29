a3e750d2e59736ca32c2a1c83052dcece98d617a
Jeysel Pacheco Bastos <jeysel@gmail.com>
2024-04-29 16:56:05 -0300

    Concluido até seção 8

--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for projeto project.
+URL configuration for project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')), 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 
@@ -6,5 +7,6 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('recipes/<int:id>/', views.recipe, name="recipe"),  
+    path('recipes/category/<int:category_id>/', views.category, name="category"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
