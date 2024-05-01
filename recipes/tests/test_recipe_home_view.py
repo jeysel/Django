7776e065806551bb657b4f3442df1f487582ad46
Jeysel Pacheco Bastos <jeysel@gmail.com>
2024-05-01 14:04:00 -0300

    Remove ultils factory

--- recipes/tests/test_recipe_home_view.py
+++ recipes/tests/test_recipe_home_view.py
@@ -1,17 +1,18 @@
 from django.urls import resolve, reverse
 from recipes import views
+
 from .test_recipe_base import RecipeTestBase
 
 
-class RecipeViewsTest(RecipeTestBase):
+class RecipeHomeViewTest(RecipeTestBase):
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
-        self.assertTrue(view.func, views.home)
-    
+        self.assertIs(view.func, views.home)
+
     def test_recipe_home_view_returns_status_code_200_OK(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertEqual(response.status_code, 200)
-    
+
     def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
@@ -23,37 +24,27 @@ class RecipeViewsTest(RecipeTestBase):
             response.content.decode('utf-8')
         )
 
+    def test_recipe_home_template_loads_recipes(self):
+        # Need a recipe for this test
+        self.make_recipe()
+
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
 
+        # Check if one recipe exists
         self.assertIn('Recipe Title', content)
-        self.assertIn('10 Minutos', content)
-        self.assertIn('5 Porções', content)
         self.assertEqual(len(response_context_recipes), 1)
 
-    def test_recipe_category_view_function_is_correct(self):
-        view = resolve(
-            reverse('recipes:category', kwargs={'category_id': 1000})
-        )
-        self.assertIs(view.func, views.category)
+    def test_recipe_home_template_dont_load_recipes_not_published(self):
+        """Test recipe is_published False dont show"""
+        # Need a recipe for this test
+        self.make_recipe(is_published=False)
 
-    def test_recipe_category_view_returns_404_if_no_recipes_found(self):
-        response = self.client.get(
-            reverse('recipes:category', kwargs={'category_id': 1000})
-        )
-        self.assertEqual(response.status_code, 404)
+        response = self.client.get(reverse('recipes:home'))
 
-    def test_recipe_detail_view_function_is_correct(self):
-        view = resolve(
-            reverse('recipes:recipe', kwargs={'id': 1})
+        # Check if one recipe exists
+        self.assertIn(
+            '<h1>No recipes found here 🥲</h1>',
+            response.content.decode('utf-8')
         )
-        self.assertIs(view.func, views.recipe)
-
-    def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
-        response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id': 1000})
-        )
-        self.assertEqual(response.status_code, 404)
-    
- 
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,8 @@
+from django.http.response import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from utils.recipes.factory import make_recipe
+from django.db.models import Q
 from recipes.models import Recipe
+
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
@@ -30,3 +32,22 @@ def recipe(request, id):
         'is_detail_page': True,
     })
 
+
+def search(request):
+    search_term = request.GET.get('q', '').strip()
+
+    if not search_term:
+        raise Http404()
+    
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) | Q(description__icontains=search_term)
+        ),
+        is_published=True
+    ).order_by('-id')
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" | ', 
+        'search_term': search_term,
+        'recipes': recipes,
+    })
