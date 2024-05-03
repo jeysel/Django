567430e8560c4c5a25925e678e47707831482eb9
Jeysel Pacheco Bastos <jeysel@gmail.com>
2024-05-03 17:07:37 -0300

    Reorganizado e Refatorado

--- authors/views.py
+++ authors/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
-from .forms import RegisterForm
+from django.contrib import messages
 from django.http import Http404
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from .forms import RegisterForm
+
+
 
 def register_view(request):
     register_form_data = request.session.get('register_form_data', None)
     form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
+        'form_action': reverse('authors:create'),
     })
 
 
@@ -17,5 +22,13 @@ def register_create(request):
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
- 
+
+    if form.is_valid():
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
+        messages.success(request, 'Your user is created, please log in.') 
+
+        del(request.session['register_form_data'])
+
     return redirect('authors:register')
